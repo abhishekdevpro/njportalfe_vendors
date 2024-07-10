1cd2d142541d526c8f3877d0f89b83747d3de7f0
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-07-10 12:58:05 +0530

    update

--- src/employeeMarkup/Pages/Verifyemailemploye.js
+++ src/employeeMarkup/Pages/Verifyemailemploye.js
@@ -1,49 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { showToastError, showToastSuccess } from "../../utils/toastify";
 
 function VerifyEmailemployee() {
   const navigate = useNavigate();
-  const token = localStorage.getItem("employeeLoginToken");
+  const token = localStorage.getItem("employeeLoginToken");  // Replace with your token retrieval logic
 
   useEffect(() => {
     const verifyEmail = async () => {
       try {
-       
         const response = await axios.get(
           "https://api.novajobs.us/api/employee/verify-account/",
-          { headers:{
-            "Content-Type": "Application/json",
-            Authorization: token,
-          } }
+          { 
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
 
         console.log(response);
 
-        if (response.data.success && response.data.success.token) {
+        if (response.data.success) {
           showToastSuccess("Email verified successfully");
           navigate("/employee");
         } else {
           showToastError("Verification failed");
-          navigate("/employee/login");
+          navigate("/employee");
         }
       } catch (error) {
         console.error("Verification Error:", error);
         showToastError("Invalid token or email");
-        navigate("/employee/login");
+        navigate("/employee");
       }
     };
 
-    if (token) {
-      verifyEmail();
-    } else {
-      navigate("/employee/login");
-    }
+    verifyEmail();
   }, [token, navigate]);
 
   return <div>Verifying...</div>;
 }
-
 
 export default VerifyEmailemployee;
